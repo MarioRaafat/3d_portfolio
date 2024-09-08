000fc16eff8df41d334fbc5f9a89f3cdd4dd173c
Mario Raafat <mariorafat10@gmail.com>
2024-09-09 02:43:52 +0300

    done

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Intro from './Intro/Intro.jsx';
+import Navbar from './Navbar/Navbar.jsx';
+import About from './About/About.jsx';
+import Tech from './Tech/Tech.jsx';
+import Experience from './Experience/Experience.jsx';
+import Works from './Work/Works.jsx';
+import Contact from './Contact/Contact.jsx';
+import Projects from './Projects/Projects.jsx';
+import Footer from './Footer/Footer.jsx';
+
+export {
+  Intro,
+  Navbar,
+  About,
+  Projects,
+  Tech,
+  Experience,
+  Works,
+  Contact,
+  Footer,
+}
